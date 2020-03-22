86f933eac7a1b68dbe0271114d3b3246599f3b4d
Chester ONeill <coneillcodes@gmail.com>
2020-03-22 16:53:15 -0400

    Store results in mongo

---
+++ bot/src/db.ts
@@ -0,0 +1,31 @@
+import { MongoClient, Db, ObjectId } from 'mongodb';
+
+// Connection URL
+const url = 'mongodb://localhost:32768';
+
+// Database Name
+const dbName = 'myproject';
+
+let db: Db;
+
+export async function connect() {
+  // Use connect method to connect to the server
+  return MongoClient.connect(url).then(client => {
+    db = client.db(dbName);
+  });
+}
+
+export const insertResults = async (
+  name: string,
+  results: { [index: string]: number }
+) => {
+  let item = db.collection('results').insertOne({ name: name, results });
+  return item;
+};
+
+export const findResults = async (id: ObjectId) => {
+  let item = await db
+    .collection('results')
+    .findOne({ _id: id }, { projection: { _id: 0 } });
+  return item;
+};
--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,29 +1,55 @@
 import { Client, Message } from 'discord.js';
+import { ObjectId } from 'mongodb';
+import { connect, findResults, insertResults } from './db';
 
 require('dotenv').config();
 
 const client = new Client();
+connect();
 
-client.on('ready', () => {
+client.on('ready', async () => {
   console.log(`Logged in as ${client?.user?.tag}!`);
 });
 
+/**
+ * run command !poll name time
+ */
 client.on('message', async (msg: Message) => {
-  if (msg.content === '!poll') {
-    await msg.channel.send('A poll has been started for 10 seconds');
+  if (msg.content.match(/^!poll/)) {
+    let groups = msg.content.match(/^!poll (?<name>.*) (?<time>.*)/)?.groups;
+    console.log(groups);
+    if (!groups) {
+      await msg.channel.send('You need to name the quiz and give a time');
+      return;
+    }
+
+    await msg.channel.send(
+      `A poll has been started for ${+groups.time || 10} seconds`
+    );
 
     let collected = await msg.channel.awaitMessages(() => true, {
-      time: 10000,
+      time: +groups.time * 1000 || 10000,
     });
 
     let totals: { [key: string]: number } = {};
     collected.forEach(item => {
-      const current = totals[item.content];
-      totals[item.content] = current || 0;
-      totals[item.content] = current + 1;
+      totals[item.content] = totals[item.content] || 0;
+      totals[item.content] = totals[item.content] + 1;
     });
 
+    insertResults(groups?.name, totals);
+
     await msg.channel.send(`Collected ${JSON.stringify(totals)}`);
+  }
+
+  if (msg.content.match(/!results .*/)) {
+    console.log(msg.content.split(' ')[1]);
+
+    const results = findResults(new ObjectId(msg.content.split(' ')[1]));
+
+    msg.channel.send(
+      `Heres the results from the poll ${JSON.stringify(results)}`
+    );
   }
 });
 
